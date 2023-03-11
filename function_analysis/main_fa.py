5d3e67ab22e4d0a8e0eccb21352da624b03ca39f
jarekstrzele <jaroslaw.strzelecki@protonmail.com>
2023-03-11 17:05:09 +0100

    Create a first main file

---
+++ function_analysis/main_fa.py
@@ -0,0 +1,17 @@
+# function analysis
+
+import sys
+from PyQt5.QtWigets import *
+
+class Main():
+    pass
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = Main()
+    main.show()
+
+    sys.exit(app.exec_())
+
